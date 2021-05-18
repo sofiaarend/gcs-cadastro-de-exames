96f0ee4842190940050f52739ff2f34d17590a31
EduScaol <eduardo.sca98@gmail.com>
2021-05-18 20:47:50 -0300

    formulario
    Desenvolvi o formulario do medico para solicitar exames

--- medico/app.js
+++ medico/app.js
@@ -1,1 +1,47 @@
 var base_url = '../';
+
+$(document).ready(function(){
+  $(".err").hide();
+  $(".check").click(function(){
+
+    if($("input:checkbox").filter(":checked").length > 1){
+      $(".err").show();
+        return false;
+  
+    } else{
+
+      $(".err").hide();
+        return true;
+    }
+
+  })
+
+})
+
+var cod = 0;
+
+function increment(){
+n++;
+return n;
+}
+
+var solicitacoes = [];
+const addSolic = (ev) => {
+  ev.preventDefault();
+  function increment();
+  let solic = {
+    id: Date.now(),
+    cod: n,
+    paciente: document.getElementById('tecno').value,
+    data: document.getElementByName('data').value,
+    exame: document.getElementById('listaExames').value,
+    med_solic: document.getElementById('usu_med').value,
+  }
+  solicitacoes.push(solic);
+  document.forms[0].reset();
+}
+document.addEventListener('DOMContentLoaded', ()=>{
+  document.getElementById('botao').addEventListener('click', addSolic);
+});
+
+
