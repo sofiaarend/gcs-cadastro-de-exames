714df6a9c851d7b13342ea3a7540115a6bd4ac7d
EduScaol <eduardo.sca98@gmail.com>
2021-05-18 22:17:01 -0300

    Corrigindo bugs
    Mudei o formato da lista de exames para deixar o formulário mais consistente

--- medico/app.js
+++ medico/app.js
@@ -1,47 +1,25 @@
 var base_url = '../';
 
-$(document).ready(function(){
-  $(".err").hide();
-  $(".check").click(function(){
-
-    if($("input:checkbox").filter(":checked").length > 1){
-      $(".err").show();
-        return false;
-  
-    } else{
-
-      $(".err").hide();
-        return true;
-    }
-
-  })
-
-})
-
-var cod = 0;
-
-function increment(){
-n++;
-return n;
-}
-
 var solicitacoes = [];
 const addSolic = (ev) => {
   ev.preventDefault();
-  function increment();
+
+  let currentDate = new Date().toISOString().split('T')[0];
   let solic = {
-    id: Date.now(),
-    cod: n,
+    id : currentDate,
     paciente: document.getElementById('tecno').value,
-    data: document.getElementByName('data').value,
-    exame: document.getElementById('listaExames').value,
-    med_solic: document.getElementById('usu_med').value,
+    dataExame: document.getElementById('data').value,
+    exame: document.getElementById('solicExames').value,
+    med: document.getElementById('solic_med').value,
   }
   solicitacoes.push(solic);
   document.forms[0].reset();
+
+  console.warn('added', {solicitacoes});
+  let pre = document.querySelector('#msg pre');
 }
 document.addEventListener('DOMContentLoaded', ()=>{
   document.getElementById('botao').addEventListener('click', addSolic);
-});
+})
 
 
