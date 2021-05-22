f0ca61323cc1383e3fa83d74e292e9ad479e0e17
williamzabot <williamzabot123@gmail.com>
2021-05-21 21:42:01 -0300

    lógica exames de pacientes

--- paciente/app.js
+++ paciente/app.js
@@ -1,1 +1,27 @@
 var base_url = '../';
+var storage = localStorage.getItem("exames")
+var exames = storage ? JSON.parse(storage) : [];
+var paciente = sessionStorage.getItem("usuario");
+console.log(exames)
+
+for (var i = 0; i > exames.length; i++) {
+   // if (exames[i].id == paciente) {
+        $('#tablePaciente tbody').append(
+           `<tr>
+                <td>${exames[i].id}</td>
+                <td>${exames[i].med}</td>
+                <td>${exames[i].paciente}</td>
+                <td>${exames[i].dataExame}</td>
+                <td>${exames[i].exame}</td>
+                <td>${exames[i].status}</td>
+            </tr>`
+        )
+    //}
+}
+
+$("#btn-paciente").click(function () {
+    var id = document.getElementById("idinput").value
+    for (var i = 0; i > exames.length; i++) {
+        exames[i].status = "inativo"
+    }
+});
